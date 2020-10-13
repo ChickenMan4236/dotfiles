8af7be76f04e8c6e4374e6d88078c499ac2e0988
ChickenMan4236 <chickenmanpl@protonmail.com>
2020-10-13 16:56:39 +0200

    test

--- setupWorkspace.sh
+++ setupWorkspace.sh
@@ -1,10 +1,13 @@
 bspc desktop --focus ^6
 bspc rule -a URxvt state=tiled
-(&>/dev/null urxvt -e "sh" -c "mpsyt; bash" &)
-sleep 1
+(&>/dev/null urxvt -e "sh" -c "vtop --theme brew; bash" &)
+sleep 0.2
 bspc node -p south
-sleep 1
 bspc node -o 0.9
-sleep 1
+sleep 0.2
 (&>/dev/null urxvt -font xft:monospace:size=1 -internalBorder 0 -e "sh" -c "vis; bash" &)
-
+sleep 0.2
+bspc node -f north
+bspc node -p east
+sleep 0.2
+(&>/dev/null urxvt green -e "sh" -c "mpsyt; bash" &)
